b073055e3812fe269e2c02097929ba4c94455fe0
Varun Saxena <r54134.varun@jnujaipur.ac.in>
2026-08-23 15:33:34 +0530

    Created functionality of calculting replinishment interval for products

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "../../.env" });
-
+dotenv.config({ path: "../.env" });
 import express from "express";
+import customersRouter from "./routes/customers.js";
 
-
-//Express Server
 const app = express();
-
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use("/api/customers", customersRouter);
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
